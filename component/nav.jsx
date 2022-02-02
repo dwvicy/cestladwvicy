f3b6cbde56ad2384d0bdc78ba81fce62e4d293bb
dwvicy <vaishdw2207@gmail.com>
2022-02-02 19:59:46 +0530

    nav bar responsive

--- component/nav.jsx
+++ component/nav.jsx
@@ -1,33 +1,34 @@
 import React from 'react';
 import {Icon} from '@iconify/react';
+import Link from 'next/link';
 
 export default function Nav() {
     return(
         <div className="nav-bar">
         <div className="parentn">
         <div class="div1n"> 
-        <span className='nav-icon'><Icon className="navy-icon "icon="noto:rocket" color="#2a2a3c" height="25"/></span>
-        <span className='label'> home </span>
+        <span className='nav-icon'><Link href='/'><a> <Icon className="navy-icon "icon="noto:rocket" color="#2a2a3c" height="25"/></a></Link></span>
+        <span className='label'> <Link href='/'><a>home</a></Link> </span>
         </div>
         <div className="div2n">
-        <span className='nav-icon'><Icon className="navy-icon "icon="noto:crown" color="#2a2a3c" height="25"/></span>
-        <span className='label'> about </span>
+        <span className='nav-icon'><Link href='/about'><a><Icon className="navy-icon "icon="noto:crown" color="#2a2a3c" height="25"/></a></Link></span>
+        <span className='label'> <Link href='/about'><a>about</a></Link> </span>
              </div>
         <div className="div3n"> 
-        <span className='nav-icon'><Icon className="navy-icon "icon="noto:briefcase" color="#2a2a3c" height="25"/></span>
-        <span className='label'> work </span>
+        <span className='nav-icon'><Link href='/work'><a><Icon className="navy-icon "icon="noto:briefcase" color="#2a2a3c" height="25"/></a></Link></span>
+        <span className='label'> <Link href='/work'><a>work</a></Link> </span>
         </div>
         <div className="div4n"> 
-        <span className='nav-icon'><Icon className="navy-icon "icon="noto:artist-palette" color="#2a2a3c" height="25"/></span>
-        <span className='label'> code </span>
+        <span className='nav-icon'><Link href='/projects'><a><Icon className="navy-icon "icon="noto:artist-palette" color="#2a2a3c" height="25"/></a></Link> </span>
+        <span className='label'> <Link href='/projects'><a>code</a></Link> </span>
         </div>
         <div className="div5n"> 
-        <span className='nav-icon'><Icon className="navy-icon "icon="noto:studio-microphone" color="#2a2a3c" height="25"/></span>
-        <span className='label'> talks </span>
+        <span className='nav-icon'><Link href='/talks'><a><Icon className="navy-icon "icon="noto:studio-microphone" color="#2a2a3c" height="25"/></a></Link></span>
+        <span className='label'> <Link href='/talks'><a>talks</a></Link> </span>
         </div>
         <div className="div6n"> 
-        <span className='nav-icon'><Icon className="navy-icon "icon="noto:bar-chart" color="#2a2a3c" height="25"/></span>
-        <span className='label'> design </span>
+        <span className='nav-icon'><Link href='/case-studies'><a><Icon className="navy-icon "icon="noto:bar-chart" color="#2a2a3c" height="25"/></a></Link></span>
+        <span className='label'> <Link href='/case-studies'><a>design</a></Link> </span>
         </div>
         </div>
         </div>
