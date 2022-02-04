a43cd27d410ad4e690e8664a4ae7560987250e81
dwvicy <vaishdw2207@gmail.com>
2022-02-04 21:11:42 +0530

    about fix

--- pages/about.jsx
+++ pages/about.jsx
@@ -16,11 +16,11 @@ export default function About() {
       <p className='boday-about'>
         Hello, I'm Vaishnavi Dwivedi. 👋 </p>
         
-        <p className='boday-about'>I'm a full-stack product designer (and Developer/Design Advocate) based in Mumbai, India. My journey into product design is quite similar to many who transitioned to this field after being a software developer. </p>
+        <p className='boday-about'>I'm a full-stack product designer (and Developer/Design Advocate) based in Mumbai, India. My journey into sproduct design is quite similar to many who transitioned to this field after being a software developer. </p>
       
-        <p className='boday-about'>I've done UX Research in every developer internship I've done since most of them were at startups. I've helped my opensource teams design several opensource projects and conducted 40+ workshops (as of Jan 2022) doing hands-on work with Figma, Design Thinking and Product Strategy.</p>
+        <p className='boday-about'>I've done UX Research in all of my developer internships since most of them were at startups. I've helped my teams design several opensource projects and conducted 40+ workshops (as of Jan 2022) specific to hands-on work with Figma, Design Thinking and Product Strategy.</p>
     
-        <p className='boday-about'>My curiousity helps me to be open-minded towards feedback, fills me with the eagerness to ask questions, and iterate towards a better product.
+        <p className='boday-about'>My curiousity helps me to be open-minded towards feedback, filliing me with the eagerness to ask questions, and iterate towards a better product.
       </p>
       </div>
       </div>
